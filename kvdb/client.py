0bd8b0357e6403e74a1c5f6268301bfc7e524b3a
Fergal Walsh <fergalwalsh@gmail.com>
2017-04-10 10:34:14 +0300

    client & server starter code

---
+++ kvdb/client.py
@@ -0,0 +1,51 @@
+import json
+import socket
+import time
+
+
+class Client(object):
+    def __init__(self, host='localhost', port=4242):
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+    def _send(self, message):
+        d = json.dumps(message)
+        self.socket.sendall(d + '\n')
+
+    def _receive(self):
+        buff = ''
+        while True:
+            buff += self.socket.recv(2048)
+            if not buff:
+                return None
+            if buff[-1] == '\n':
+                break
+        message = json.loads(buff)
+        return message
+
+    def send(self, message):
+        try:
+            self._send(message)
+        except Exception:
+            self.connect()
+            self._send(message)
+        result = self._receive()
+        self.socket.close()
+        return result
+
+    def disconnect(self):
+        self.socket.close()
+
+
+if __name__ == '__main__':
+    client = Client()
+    while True:
+        r = client.send({'command': 'PING'})
+        if r:
+            print(r)
+        time.sleep(1)
---
+++ kvdb/dbserver.py
@@ -0,0 +1,55 @@
+import json
+import socket
+
+
+class Server(object):
+    def __init__(self, host='localhost', port=4242):
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def run(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # bind the socket to a public host, and a well-known port
+        self.socket.bind((self.host, self.port))
+        # become a server socket
+        self.socket.listen(5)
+
+        while True:
+            # accept connections from outside
+            (clientsocket, address) = self.socket.accept()
+            print('Client connected: %s' % (address,))
+            # now do something with the clientsocket
+            self.handle_client(clientsocket)
+
+    def handle_client(self, client):
+        message = self._receive(client)
+        result = self.handle_message(message)
+        d = json.dumps(result)
+        client.sendall(d + '\n')
+
+    def _receive(self, client):
+        buff = ''
+        while True:
+            buff += client.recv(2048)
+            if not buff:
+                # connection has been closed
+                return None
+            # messages are delimited by \n
+            if buff[-1] == '\n':
+                break
+        buff = buff[:-1]
+        print(buff)
+        message = json.loads(buff)
+        return message
+
+    def handle_message(self, message):
+        if message['command'] == 'PING':
+            return {'result': 'PONG', 'status': 'OK'}
+        return {'message': 'Unknown command', 'status': 'ERROR'}
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
