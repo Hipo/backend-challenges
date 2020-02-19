3bd23eab751ecfbb68ba24cc92172def455c3bb6
cemonel <cemonel97@gmail.com>
2020-02-19 16:34:09 +0300

    project structure revision

--- task_queue/demo/wsgi.py
+++ task_queue/demo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_queue project.
+WSGI config for demo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_queue.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
 
 application = get_wsgi_application()
--- task_queue/tools/run_development.sh
+++ task_queue/tools/run_development.sh
@@ -3,7 +3,7 @@
 # This script starts the development environment using Docker
 # Launch as: source tools/run_development.sh from the project's root
 
-SERVICE_NAME="task_queue"
+SERVICE_NAME="demo"
 
 CONTAINER_UID=$(id -u) CONTAINER_GID=$(id -g) docker-compose stop
 CONTAINER_UID=$(id -u) CONTAINER_GID=$(id -g) docker-compose rm --force
